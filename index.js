017b652030f25340294eded864b2901ff0cbb3d0
Sanskar Bhusal <sanskarbhusal123@gmail.com>
2025-11-22 21:40:46 +0545

    feat: add player The player can be moved left and right

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 import EnemyController from "./controllers/EnemyController.js"
-
+import PlayerController from "./controllers/PlayerController.js"
 
 const canvas = document.getElementById("my-canvas")
 const ctx = canvas.getContext("2d")
@@ -11,10 +11,12 @@ const background = new Image()
 background.src = "images/space.png"
 
 const enemeyController = new EnemyController(canvas)
+const playerController = new PlayerController(canvas, 3)
 
 function game() {
     ctx.drawImage(background, 0, 0, canvas.width, canvas.height)
-    enemeyController.draw(ctx)
+    // enemeyController.draw(ctx)
+    playerController.draw(ctx)
 }
 
-setInterval(game, 1000 / 30);
+setInterval(game, 1000 / 60);
