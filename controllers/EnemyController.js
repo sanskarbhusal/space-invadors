e02fab4154256b3a7e89fcbc14d9d103e593f6ad
Sanskar Bhusal <sanskarbhusal123@gmail.com>
2025-11-22 15:30:45 +0545

    feat: add enemy controller

---
+++ controllers/EnemyController.js
@@ -0,0 +1,39 @@
+import Enemy from "../utils/Enemy.js"
+export default class EnemyController {
+
+    enemyMap = [
+        [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+        [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+        [2, 2, 2, 3, 3, 3, 3, 2, 2, 2],
+        [2, 2, 2, 3, 3, 3, 3, 2, 2, 2],
+        [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+        [2, 2, 2, 2, 2, 2, 2, 2, 2, 2]
+    ]
+
+    enemyRows = []
+
+    constructor(canvas) {
+        this.convas = canvas
+    }
+
+    draw(ctx) {
+        this.drawEnemies(ctx)
+    }
+
+    drawEnemies(ctx) {
+        this.enemyRows.flat().forEach((enemy) => {
+            enemy.draw(ctx)
+        })
+    }
+
+    createEnemies() {
+        this.enemyMap.forEach((row, rowIndex) => {
+            this.enemyRows[rowIndex] = []
+            row.forEach((enemyNumber, enemyIndex) => {
+                if (enemyNumber > 0) {
+                    this.enemyRows[rowIndex].push(new Enemy(enemyIndex * 50, rowIndex * 35, enemyNumber))
+                }
+            })
+        })
+    }
+}
