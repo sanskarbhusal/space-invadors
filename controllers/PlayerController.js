a5efa060da127acc0f48ac2f552353829bf464cd
Sanskar Bhusal <sanskarbhusal123@gmail.com>
2025-11-24 15:15:11 +0545

    fix: update resource path     Turns out, in production the file urls are sent to the static file server and unfortunately, they can't can't resolve relative paths that contain '../'. So, I had to use absolute path instead for the file URL.     Note: You can still use '../' in while importing since there it is handled by the JS engine before sending request to the static file server to fetch the requested .js file

--- controllers/PlayerController.js
+++ controllers/PlayerController.js
@@ -17,7 +17,7 @@ export default class PlayerController {
         this.width = 50
         this.height = 48
         this.image = new Image()
-        this.image.src = "../images/player.png"
+        this.image.src = "images/player.png"
 
         document.addEventListener("keydown", this.keydown)
         document.addEventListener("keyup", this.keyup)
